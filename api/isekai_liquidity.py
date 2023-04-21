3ef72f4d632708af1fb0480a6d8b68069a78dcbd
dniminenn <85250163+dniminenn@users.noreply.github.com>
2023-04-21 02:48:58 -0300

    add more liq and mktcap

---
+++ api/isekai_liquidity.py
@@ -0,0 +1,23 @@
+from http.server import BaseHTTPRequestHandler
+from urllib.parse import urlparse, parse_qs
+import requests
+import json
+
+
+def fetch_dexscreener_data():
+    url = "https://api.dexscreener.com/latest/dex/pairs/polygon/0x47130c5227e478c393288295fc4d975580bd9cdf"
+    response = requests.get(url)
+    return json.loads(response.text)
+
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        data = fetch_dexscreener_data()
+        liquidity = data.get("pair", {}).get("liquidity", {}).get("usd", "")
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.send_header('Cache-Control', 's-maxage=300, stale-while-revalidate')
+        self.end_headers()
+        self.wfile.write(str(liquidity).encode())
+        return
